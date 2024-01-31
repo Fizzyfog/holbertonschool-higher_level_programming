d19c95f26568f9d6d839672cc0448f7cb06c2aab
Fizzyfog <idrissisarah@gmail.com>
2024-01-31 13:15:08 +0100

    Refactor print_matrix_integer function to handle end of line whitespace

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+
     for row in matrix:
-        for column in row:
-            print("{:d}".format(column), end=" ")
+        for integer in range(len(row)):
+            if integer < len(row) - 1:
+                print("{:d}".format(row[integer]), end=" ")
+            else:
+                print("{:d}".format(row[integer]), end="")
         print()
