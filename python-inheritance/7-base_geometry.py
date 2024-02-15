3c862a2e3c683f980935c37477f9c222c8735cb2
Fizzyfog <idrissisarah@gmail.com>
2024-02-15 21:07:02 +0100

    Refactor type check and error message in BaseGeometry class constructor

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -18,7 +18,7 @@ class BaseGeometry:
             TypeError: If `value` isn't a integer.
             ValueError: If `value` is less than or equal to zero.
         """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
